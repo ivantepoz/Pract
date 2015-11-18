283b842b874596dfc7ecd6e39588bce5d6491300
ivantepoz <damcb164e37@gmail.com>
2015-11-18 12:14:58 -0600

    subida2

---
+++ js/acciones.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+function consulta()
+{
+	datos="";
+	$.ajax({
+		type:"POST",
+		url: "192.168.1.30/procesar.php",
+		datos: datos
+	}).done(function(mensaje){
+		alert (mensaje);
+	});
+}
+
+
+
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	$("#btnconsulta").on("tap", function(){
+		alert("Preguntando...");
+		consulta();
+	});
+	
+}); 
+});
+
